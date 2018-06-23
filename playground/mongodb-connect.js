65f4e021339fcbd8ee91381ce5160b3a8c16d59c
Nour El-Rashidy <elrashidyn@gmail.com>
2018-06-23 20:55:28 +0200

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if(error){
+    console.log('Unable to connect to Mongodb server!');
+    return;
+  }
+
+  console.log('Connected to Mongodb server!');
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, res) => {
+  //   if(err){
+  //     console.log('Unable to insert todo', err);
+  //     return;
+  //   }
+  //
+  //   console.log(JSON.stringify(res.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'nour',
+    age: 22,
+    location: 'cairo'
+  }, (err, res) => {
+    if(err){
+      console.log('Unable to insert to Users collection!', err);
+      return;
+    }
+    console.log('Inserted to Users collection suuccessfully!');
+    console.log(JSON.stringify(res.ops, undefined, 2));
+  });
+
+  db.close();
+});
